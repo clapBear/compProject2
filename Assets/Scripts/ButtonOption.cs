76dc42b55134ace3f38db1f0e2a63f8cf459da09
YueFan Wu <13456147198@163.com>
2019-10-13 15:39:09 +1100

    yuefan
    yuefan

---
+++ Assets/Scripts/ButtonOption.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ButtonOption : MonoBehaviour
+{
+ public void PlayGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+  public void TrackSelect()
+    {
+        SceneManager.LoadScene(1);
+    }
+  public void MainMenu ()
+    {
+        SceneManager.LoadScene(0);
+    }
+    // below here are track selections buttons
+  public void Track01()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ Assets/Scripts/CarActive.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class CarActive : MonoBehaviour
+{
+
+	public GameObject CarControl;
+
+	void Start()
+	{
+		CarControl.GetComponent<CarUserControl>().enabled = true;
+	}
+}
