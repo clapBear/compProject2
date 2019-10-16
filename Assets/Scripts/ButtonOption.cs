3d5ba43de2e91800d11581c39c347014b6fcb19d
CHENG WANG <chengw4@student.unimelb.edu.au>
2019-10-17 02:19:16 +1100

    ???

--- Assets/Scripts/ButtonOption.cs
+++ Assets/Scripts/ButtonOption.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ButtonOption : MonoBehaviour
 {
- public void PlayGame()
-    {
-        SceneManager.LoadScene(2);
-    }
-  public void TrackSelect()
+ //public void PlayGame()
+   // {
+     //   SceneManager.LoadScene(2);
+    //}
+  public void PlayGame()
     {
         SceneManager.LoadScene(1);
     }
@@ -20,5 +20,13 @@ public class ButtonOption : MonoBehaviour
   public void Track01()
     {
         SceneManager.LoadScene(2);
+    }
+    public void Track02()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Track03()
+    {
+        SceneManager.LoadScene(4);
     }
 }
