de330da338b0d6417b57f7d33947a1f1c81819ca
YueFan Wu <13456147198@163.com>
2019-10-27 13:32:47 +1100

    completed

--- Assets/Scripts/ButtonOption.cs
+++ Assets/Scripts/ButtonOption.cs
@@ -4,31 +4,36 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ButtonOption : MonoBehaviour
 {
-   
+
     public void PlayGame()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(1);
     }
     public void MainMenu ()
     {
+      Time.timeScale = 1;
         SceneManager.LoadScene(0);
     }
     // below here are track selections buttons
     public void Track01()
     {
+      Time.timeScale = 1;
         SceneManager.LoadScene(2);
     }
     public void Track02()
     {
+      Time.timeScale = 1;
         SceneManager.LoadScene(3);
     }
     public void Track03()
     {
+      Time.timeScale = 1;
         SceneManager.LoadScene(4);
     }
     public void QuitGame()
     {
         Application.Quit();
     }
-  
+
 }
